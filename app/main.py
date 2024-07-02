ecb7e7a525446533524ab40a28da2ccc6e332697
Tony Kanyamuka <tonykanyamuka@gmail.com>
2024-07-02 12:30:51 +0200

    created a endpoint to get a single product description

--- app/main.py
+++ app/main.py
@@ -3,18 +3,33 @@ from database.models import Product
 from database.schemas import all_products, individual_product
 from config import db_product_table
 from utils import generate_description
+from bson.objectid import ObjectId
 
 app = FastAPI()
 todo_router = APIRouter()
 # app.include_router(todo_router, prefix="/api/v1/todos")
+
+# The endpoint to get all products description
 
 
 @app.get("/products")
 async def get_all_products_description():
     return all_products(db_product_table.find())
 
+# endpoint to get a single product description
+
 
 @app.get("/products/{product_id}")
+async def get_single_product_description(product_id: str):
+    id = ObjectId(product_id)
+    existing_product_description = db_product_table.find_one(
+        {"_id": id})
+    if not existing_product_description:
+        return HTTPException(status_code=404, detail=f"Product does not exist")
+    return individual_product(existing_product_description)
+
+
+# an endpoint to generate the product description and store in the db
 @app.post("/products")
 async def generate_product_description(product: Product):
     description = generate_description(
